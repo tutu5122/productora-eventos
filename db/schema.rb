13c0bc96859325fe845f2ca78397d189e2b7c1ea
Cristobal Dominguez <cristobald@gmail.com>
2022-07-01 21:14:00 -0400

    Realiza migraci

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_01_224342) do
+ActiveRecord::Schema.define(version: 2022_07_02_011157) do
 
   create_table "bands", force: :cascade do |t|
     t.string "name"
@@ -32,5 +32,15 @@ ActiveRecord::Schema.define(version: 2022_07_01_224342) do
     t.index ["band_id"], name: "index_concerts_on_band_id"
   end
 
+  create_table "crews", force: :cascade do |t|
+    t.string "name"
+    t.string "instrument"
+    t.integer "band_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["band_id"], name: "index_crews_on_band_id"
+  end
+
   add_foreign_key "concerts", "bands"
+  add_foreign_key "crews", "bands"
 end
